a046e7f139cb52ddb30b95f6e54070ec5c281a3a
Ivan Franchin <ivan.franchin@takeaway.com>
2024-03-31 21:57:59 +0200

    Using RestClient instead of RestTemplate

--- movie-ui/src/main/java/com/ivanfranchin/movieui/client/MovieApiClientConfig.java
+++ movie-ui/src/main/java/com/ivanfranchin/movieui/client/MovieApiClientConfig.java
@@ -3,10 +3,9 @@ package com.ivanfranchin.movieui.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.support.RestTemplateAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class MovieApiClientConfig {
@@ -16,9 +15,8 @@ public class MovieApiClientConfig {
 
     @Bean
     public MovieApiClient movieApiClient() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(movieApiUrl));
-        RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplate);
+        RestClient restClient = RestClient.builder().baseUrl(movieApiUrl).build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(MovieApiClient.class);
     }
