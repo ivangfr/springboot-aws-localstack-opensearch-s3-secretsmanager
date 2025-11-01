664d60a17054228b69c08d34b91e1bdacfbdb28b
Ivan Franchin <ivan.franchin@takeaway.com>
2025-11-01 09:35:06 +0100

    Project Upgrade
    - Upgrade to Spring Boot 3.5.7
    - Upgrade wrapperVersion to 3.3.4 and Apache Maven to 3.9.11
    - Upgrade to SpringDoc OpenAPI 2.8.13
    - Upgrade to Localstack Docker image 4.10.0
    - Upgrade to amazon-awssdk 2.36.3
    - Upgrade to spring-cloud-aws 3.4.0
    - Upgrade to opensearch-client 3.2.0

--- movie-api/src/main/java/com/ivanfranchin/movieapi/aws/OpenSearchConfig.java
+++ movie-api/src/main/java/com/ivanfranchin/movieapi/aws/OpenSearchConfig.java
@@ -1,11 +1,13 @@
 package com.ivanfranchin.movieapi.aws;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.http.HttpHost;
+import org.apache.hc.core5.http.HttpHost;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URISyntaxException;
 
 @RequiredArgsConstructor
 @Configuration
@@ -14,7 +16,7 @@ public class OpenSearchConfig {
     private final AwsProperties awsProperties;
 
     @Bean
-    RestHighLevelClient restHighLevelClient() {
+    RestHighLevelClient restHighLevelClient() throws URISyntaxException {
         return new RestHighLevelClient(
                 RestClient.builder(HttpHost.create(awsProperties.getEndpoint()))
                         .setPathPrefix(String.format("/opensearch/%s/%s",
